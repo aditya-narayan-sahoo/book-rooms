48b310f4254332dffc03eaa522c30b0aa4163412
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-10-08 21:17:35 +0530

    Add Header Component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
-import "../assets/styles/globals.css";
-
+import "@/assets/styles/globals.css";
+import Header from "@/components/Header";
 const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Book Rooms",
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header />
         <main className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
           {children}
         </main>

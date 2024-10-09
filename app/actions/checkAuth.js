3ca11f11b282d1e1465c4313b5df75ae6738dca5
Aditya Narayan Sahoo <adityasahoo246@gmail.com>
2024-10-10 00:46:14 +0530

    Finish middleware config

--- app/actions/checkAuth.js
+++ app/actions/checkAuth.js
@@ -9,12 +9,13 @@ async function checkAuth() {
   }
   try {
     const { account } = await createSessionClient(sessionCookie.value);
-    const user = account.get();
+    const user = await account.get();
     return {
       isAuthenticated: true,
       user: { id: user.$id, name: user.name, email: user.email },
     };
   } catch (error) {
+    console.error("Authentication error: ", error);
     return {
       isAuthenticated: false,
     };
--- middleware.js
+++ middleware.js
@@ -2,13 +2,17 @@ import { NextResponse } from "next/server";
 import checkAuth from "./app/actions/checkAuth";
 
 export async function middleware(request) {
-  const { isAuthenticated } = await checkAuth();
-  if (!isAuthenticated) {
-    return NextResponse.redirect(new URL("/login", request.url));
+  try {
+    const { isAuthenticated } = await checkAuth();
+    if (!isAuthenticated) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+    return NextResponse.next();
+  } catch (error) {
+    console.error("Middleware error:", error);
+    return NextResponse.redirect(new URL("/error", request.url));
   }
-  return NextResponse.next();
 }
-
 export const config = {
   matcher: ["/bookings"],
 };
